7e8db273b1a1ec1959e6a336053ffb704a788837
alex-ux <alexandr20081002@gmail.com>
2021-05-26 11:24:14 +0700

    Finish

---
+++ app/build/generated/source/buildConfig/debug/com/alexux/alexux/a3000/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.alexux.alexux.a3000;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.alexux.alexux.a3000";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
